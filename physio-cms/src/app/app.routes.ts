ed341bf2f28c49be5d7dc1f080c1d1deabfab6d2
P33J3 <peterjonlawrence@gmail.com>
2023-11-24 23:39:23 -0500

    - add client form created - input data is being collected

--- physio-cms/src/app/app.routes.ts
+++ physio-cms/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Route } from '@angular/router';
 import {ClientListComponent} from "./components/clients/client-list/client-list.component";
+import {AddClientComponent} from "./components/clients/add-client/add-client.component";
 
 export const appRoutes: Route[] = [
     {
@@ -11,4 +12,8 @@ export const appRoutes: Route[] = [
         path: 'home',
         component: ClientListComponent
     },
+    {
+        path: 'addclient',
+        component: AddClientComponent
+    }
 ];
--- physio-cms/src/app/components/clients/add-client/add-client.component.ts
+++ physio-cms/src/app/components/clients/add-client/add-client.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 
+// interface clientForm {
+//   firstName: string;
+//   lastName: string;
+//   age: string;
+//   gender: {
+//     gendermale: string;
+//     genderfemale: string;
+//   };
+//   condition: string;
+//   status: string;
+//   weight: string;
+// }
 @Component({
   selector: 'physio-cms-add-client',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './add-client.component.html',
   styleUrl: './add-client.component.css',
 })
-export class AddClientComponent {}
+export class AddClientComponent {
+  clientForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.clientForm = this.fb.group({
+      firstName: ["", Validators.required],
+      lastName: ["", Validators.required],
+      age: ["", Validators.required],
+      gender: ["", Validators.required],
+      condition: ["", Validators.required],
+      status: ["", Validators.required],
+      weight: ["", Validators.required],
+      // weight: ['', Validators.required]
+    })
+  }
+
+  onSubmit() {
+    console.log("Form submitted", this.clientForm.value);
+  }
+
+
+}
