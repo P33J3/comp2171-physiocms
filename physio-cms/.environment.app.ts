ec32584916e2d0b1842ce60ff391b6a62ecb0b95
P33J3 <peterjonlawrence@gmail.com>
2024-04-01 12:44:26 -0500

    -included missing files

--- physio-cms/.environment.app.ts
+++ physio-cms/.environment.app.ts
@@ -1,6 +1,6 @@
 
-// export const localhost = 'http://localhost:9000';
-export const localhost = 'https://comp2140-physiocms-service.onrender.com';
+export const localhost = 'http://localhost:9000';
+// export const localhost = 'https://comp2140-physiocms-service.onrender.com';
 
 const apiKey= "AIzaSyA6_f4v7_xcONz7pNHShi0g8kMJBBMTvWA";
 const authDomain= "physio-cms-backend.firebaseapp.com";
@@ -9,6 +9,7 @@ const storageBucket= "physio-cms-backend.appspot.com";
 const messagingSenderId= "907508896460";
 const appId= "1:907508896460:web:e4d6099e8c200af916a869";
 const measurementId= "G-5VBESZ3F82";
+const serviceKey='../physio-cms-backend-firebase-adminsdk-gfftx-f514aebb12.json'
 export const environment = {
   firebaseConfig : {
     apiKey,
@@ -17,7 +18,8 @@ export const environment = {
     storageBucket,
     messagingSenderId,
     appId,
-    measurementId
+    measurementId,
+    serviceKey
   },
   localhost
 
--- physio-cms/server/firebaseConfig.mjs
+++ physio-cms/server/firebaseConfig.mjs
@@ -2,13 +2,14 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection } from 'firebase/firestore';
 import { config } from 'dotenv';
-import * as environment  from '../environment.mjs';
+import * as environment  from '../.environment.mjs';
 
 config();
 
 const app = initializeApp(environment.firebaseConfig);
 const db = getFirestore(app);
 export const clientsRef = collection(db, 'Clients');
+export const calendarRef = collection(db, 'calendar');
  // module.exports = clientsRef;
 
 
