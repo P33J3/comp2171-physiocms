4fa63a19fa63e2a9238033f4654043927b3c1fda
P33J3 <peterjonlawrence@gmail.com>
2024-04-01 13:01:05 -0500

    -updated more missing files

---
+++ physio-cms/.environment.mjs
@@ -0,0 +1,25 @@
+
+
+
+const apiKey= "AIzaSyA6_f4v7_xcONz7pNHShi0g8kMJBBMTvWA";
+const authDomain= "physio-cms-backend.firebaseapp.com";
+const projectId= "physio-cms-backend";
+const storageBucket= "physio-cms-backend.appspot.com";
+const messagingSenderId= "907508896460";
+const appId= "1:907508896460:web:e4d6099e8c200af916a869";
+const measurementId= "G-5VBESZ3F82";
+const redundantKey = "fdgefheghetg";
+
+export const firebaseConfig={
+    apiKey,
+    authDomain,
+    projectId,
+    storageBucket,
+    messagingSenderId,
+    appId,
+    measurementId,
+    redundantKey
+};
+
+
+
